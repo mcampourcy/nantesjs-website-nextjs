af2881015846396c63994c64066ed2fef870bed4
mcampourcy <11388201+mcampourcy@users.noreply.github.com>
2024-02-04 14:55:08 +0100

    :recycle: Improve main layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,9 @@
-import { Header } from '@/app/components/Header'
-
 export default async function Page () {
 
     return (
         <>
-            <Header />
-            <main>
-                <h1>Ceci est un titre</h1>
-                <p>Ceci est un texte</p>
-            </main>
+            <h1>Ceci est un titre</h1>
+            <p>Ceci est un texte</p>
         </>
     )
 
