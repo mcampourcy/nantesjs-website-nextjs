9f8e95cf8a3136512ee475d67af1ee24b46217ae
mcampourcy <11388201+mcampourcy@users.noreply.github.com>
2024-11-24 14:51:48 +0100

    🚛 Remove useless files

--- src/app/meetups/[id]/page.js
+++ src/app/meetups/[id]/page.js
@@ -1,10 +1,13 @@
-import { getMeetupData } from '@/lib/getSortedMeetupListByYear.js'
-import { MeetupDetails } from '@/app/components'
+import { getMeetupData } from '@/lib'
+import { MeetupDetails, MeetupTitle } from '@/app/components'
 
-// Set the title of the page to be the post title, note that we no longer use
-// e.g. next/head in app dir, and this can be async just like the server
+/*
+ * Set the title of the page to be the post title, note that we no longer use
+ * e.g. next/head in app dir, and this can be async just like the server
+ */
 export async function generateMetadata ({ params }) {
-    const postData = await getMeetupData(params.id)
+    const { id } = await params
+    const postData = await getMeetupData(id)
 
     return {
         title: postData.title
@@ -12,11 +15,13 @@ export async function generateMetadata ({ params }) {
 }
 
 export default async function Post ({ params }) {
-    const meetup = await getMeetupData(params.id)
+    const { id } = await params
+    const meetup = await getMeetupData(id)
 
     return (
-        <div className="block-fullwidth">
-            <MeetupDetails meetup={meetup}/>
-        </div>
+        <>
+            <MeetupTitle meetup={meetup} />
+            <MeetupDetails meetup={meetup} />
+        </>
     )
 }
--- src/app/meetups/page.js
+++ src/app/meetups/page.js
@@ -2,11 +2,25 @@ import Link from 'next/link'
 import { getSortedMeetupListByYear } from '@/lib/getSortedMeetupListByYear.js'
 
 export default async function Page () {
-    const sortedMeetups = getSortedMeetupListByYear()
+    const posts = getSortedMeetupListByYear()
 
     return (
         <div>
-            <h1>Meetups</h1>
+            <h1>My blog</h1>
+
+            <h2>All posts:</h2>
+            <ul>
+                {posts.map((post) => {
+                    const { id, date, title } = post
+                    return (
+                        <li key={id}>
+                            <Link href={`/meetups/${id}`}>
+                                {date} - {title}
+                            </Link>
+                        </li>
+                    )
+                })}
+            </ul>
         </div>
     )
 }
